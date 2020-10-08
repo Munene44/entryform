a7641db2ceada1c759c23035f941b1d5a79d1cac
nenekelvin <karvinmitchar@gmail.com>
2020-10-08 09:42:11 +0300

    image column

--- db/migrate/20201008063307_add_field_to_tablename.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldToTablename < ActiveRecord::Migration[6.0]
-  def change
-    add_column :tablenames, :image, :string
-  end
-end
--- db/migrate/20201008063958_add_field_to_entries.rb
+++ db/migrate/20201008063958_add_field_to_entries.rb
@@ -1,4 +1,4 @@
-class AddFieldToEntry < ActiveRecord::Migration[6.0]
+class AddFieldToEntries < ActiveRecord::Migration[6.0]
   def change
     add_column :entries, :image, :string
   end
